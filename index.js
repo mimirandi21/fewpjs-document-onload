e14c2e63eab33ba1fbe7846b1dae6fa8e29ff748
Randi Schreiner <mimirandi21@gmail.com>
2021-01-08 11:29:50 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+	let textLine = document.querySelector("p#text");
+	textLine.innerHTML = "This is really cool!";
+});
